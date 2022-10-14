fa4af81a3fb6b6faf9607311e031717408e184be
Ron Almog <ron8132@gmail.com>
2022-10-13 20:16:58 -0400

    index

---
+++ pages/New.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const New = (props: Props) => {
+  return <div>New</div>;
+};
+
+export default New;
---
+++ pages/Single.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Single = (props: Props) => {
+  return <div>Single</div>;
+};
+
+export default Single;
